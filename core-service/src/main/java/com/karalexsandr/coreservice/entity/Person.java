05f44f24a2420e8964fc019ef1137d799664bbb9
Winterom <winterom@gmail.com>
2022-07-19 16:52:37 +0300

    Описаны все сущности Core Service

---
+++ core-service/src/main/java/com/karalexsandr/coreservice/entity/Person.java
@@ -0,0 +1,29 @@
+package com.karalexsandr.coreservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "persons")
+@Getter
+@Setter
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @OneToMany(mappedBy = "teacher")
+    private List<ActiveCourse> activeCourses;
+}
