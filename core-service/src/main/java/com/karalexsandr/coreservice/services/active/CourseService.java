fba0a180a6b273e6a12e874feee56073a7fcfdc5
prumix <prumixxx@gmail.com>
2022-08-01 09:42:55 +0200

    добавил интеграцию auth-service

--- core-service/src/main/java/com/karalexsandr/coreservice/services/active/CourseService.java
+++ core-service/src/main/java/com/karalexsandr/coreservice/services/active/CourseService.java
@@ -6,12 +6,15 @@ import com.karalexsandr.coreservice.entity.CourseTemplate;
 import com.karalexsandr.coreservice.entity.Person;
 import com.karalexsandr.coreservice.entity.StreamTemplate;
 import com.karalexsandr.coreservice.exception.CoreException;
+import com.karalexsandr.coreservice.integrations.AuthServiceIntegration;
 import com.karalexsandr.coreservice.repository.CourseRepository;
 import com.karalexsandr.coreservice.services.PersonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import web.entity.ERole;
+import web.entity.ProfileDto;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +26,13 @@ public class CourseService {
     private final CourseRepository repository;
     private final LessonService lessonService;
     private final PersonService personService;
+    private final AuthServiceIntegration authServiceIntegration;
 
 
     @Transactional
-    public List<Course> createCoursesForStreamTemplate(StreamTemplate streamTemplate){
+    public List<Course> createCoursesForStreamTemplate(StreamTemplate streamTemplate) {
         List<Course> courses = streamTemplate.getCourseTemplates().stream()
-                .map(ct ->{
+                .map(ct -> {
                     Course course = new Course();
                     course.setTitle(ct.getTitle());
                     course.setCourseTemplate(ct);
@@ -41,12 +45,15 @@ public class CourseService {
     }
 
     public void setTeacher(Long idCourse, Long idTeacher) {
-
         Person person = personService.getById(idTeacher);
-        /*Тут надо бы проверить а есть ли роль учителя у этого пользователя*/
-        int i = repository.setTeacher(person, idCourse);
-        if(i==0){
-            throw new CoreException("Не удалось припязать учителя с id: "+ idTeacher);
+        if (person !=null) {
+            ProfileDto profileDto = authServiceIntegration.getUserById(idTeacher);
+            if (profileDto.getRoles().contains(ERole.ROLE_TEACHER)) {
+                int i = repository.setTeacher(person, idCourse);
+                if (i == 0) {
+                    throw new CoreException("Не удалось припязать учителя с id: " + idTeacher);
+                }
+            }
         }
     }
 }
